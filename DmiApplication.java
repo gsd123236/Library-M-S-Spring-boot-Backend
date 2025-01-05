fac2b5122cc493e2e56fc914fa643c667b1e0b60
gsd123236 <gauravdhotre2009@gmail.com>
2025-01-05 19:22:05 +0530

    Add files via upload

---
+++ DmiApplication.java
@@ -0,0 +1,13 @@
+package mca.dmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmiApplication.class, args);
+	}
+
+}
